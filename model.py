570376800b6dcf2b5db435c4dad136797503053e
cvprkang <44186318+cvprkang@users.noreply.github.com>
2022-03-03 18:14:33 +0800

    Update model.py

--- model.py
+++ model.py
@@ -3,48 +3,6 @@ from collections import OrderedDict
 import math
 import numpy as np
 
-
-
-
-
-class ConvNet(nn.Module):
-    def __init__(self, opt, momentum=0.1, affine=True, track_running_stats=True):
-        super(ConvNet, self).__init__()
-        self.in_planes  = opt['in_planes']
-        self.out_planes = opt['out_planes']
-        self.num_stages = opt['num_stages']
-        if type(self.out_planes) == int:
-            self.out_planes = [self.out_planes for i in range(self.num_stages)]
-        assert(type(self.out_planes)==list and len(self.out_planes)==self.num_stages)
-
-        num_planes = [self.in_planes,] + self.out_planes
-        userelu = opt['userelu'] if ('userelu' in opt) else True
-
-        conv_blocks = []
-        for i in range(self.num_stages):
-            if i == (self.num_stages-1):
-                conv_blocks.append(
-                    ConvBlock(num_planes[i], num_planes[i+1], userelu=userelu))
-            else:
-                conv_blocks.append(
-                    ConvBlock(num_planes[i], num_planes[i+1]))
-        self.conv_blocks = nn.Sequential(*conv_blocks)
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-
-    def forward(self, x):
-        out = self.conv_blocks(x)
-        out = out.view(out.size(0),-1)
-        return out
-
-
-
 # encoder for imagenet dataset
 class EmbeddingImagenet(nn.Module):
     def __init__(self,
